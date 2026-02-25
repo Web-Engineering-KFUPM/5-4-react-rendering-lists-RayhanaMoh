f89593d088f7fec5fac65dff79342db770d6a50c
RayhanaMoh <s202243580@kfupm.edu.sa>
2026-02-25 23:20:53 +0300

    Task3

--- 5-4-react-rendering-lists/src/components/TaskItem.jsx
+++ 5-4-react-rendering-lists/src/components/TaskItem.jsx
@@ -10,12 +10,12 @@ export default function TaskItem({ task, onToggle, onDelete }) {
         <input type="checkbox" />
 
         {/* TODO (TASK 3): Show <DueBadge /> ONLY when task is NOT done (&&)
-            Example:
-              {!task.isDone && <DueBadge dueDate={task.dueDate} />} */}
+            Example:*/}
+          {!task.isDone && <DueBadge dueDate={task.dueDate} />}
 
         {/* TODO (TASK 2): Display the task title
-            Example:
-              <span className="title">{task.title}</span> */}
+            Example:*/}
+              <span className="title">{task.title}</span>
       </label>
 
       {/* TODO (TASK 4): Delete button calls onDelete(task.id) on click */}
